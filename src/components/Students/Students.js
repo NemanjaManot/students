c22722d0714eaee0428b65583e0206f663b037d3
Djole <djordje.zivkovic@htecgroup.com>
2018-01-23 16:44:25 +0100

    added radio buttons, style

--- src/components/Students/Students.js
+++ src/components/Students/Students.js
@@ -9,7 +9,10 @@ import './Students.scss';
 
 class Students extends React.Component {
     state = {
-        students: []
+        students: [],
+        here: 0,
+        nothere: 0,
+        late: 0
     };
 
     componentDidMount() {
@@ -20,8 +23,34 @@ class Students extends React.Component {
         });
     }
 
+    studentStatus(e) {
+        let here = this.state.here;
+        let nothere = this.state.nothere;
+        let late = this.state.late;
+        if (e === 'here') {
+            here++;
+            this.setState({
+                here: here
+            })
+        } else if (e === 'nothere') {
+            nothere++;
+            this.setState({
+                nothere: nothere
+            })
+        } else if (e === 'late') {
+            late++;
+            this.setState({
+                late: late
+            })
+        }
+    }
+
+
     render() {
         let allStudents = this.state.students;
+        const studentColor = allStudents.map(student => student.attendanceMark.present);
+        console.log(studentColor);
+
         return (
             <div>
                 { allStudents.map(student =>
@@ -29,12 +58,24 @@ class Students extends React.Component {
                         <p>{ student.firstName } { student.lastName }</p>
                         <img src={ student.image }/>
                         <form className="formselector">
-                            <input type="radio" value="1" name="formsel"/>
-                            <input type="radio" value="2" name="formsel"/>
-                            <input type="radio" value="3" name="formsel"/>
+                            <span>Here:</span><input onChange={ this.studentStatus.bind(this, 'here') }
+                                                     type="radio"
+                                                     value="here"
+                                                     name="formsel"/>
+                            <span>Not Here:</span><input onChange={ this.studentStatus.bind(this, 'nothere') }
+                                                         type="radio"
+                                                         value="nothere"
+                                                         name="formsel"/>
+                            <span>Late:</span><input onChange={ this.studentStatus.bind(this, 'late') }
+                                                     type="radio"
+                                                     value="late"
+                                                     name="formsel"/>
                         </form>
                     </div>
                 ) }
+                <p> Here: { this.state.here }</p>
+                <p> Not here: { this.state.nothere }</p>
+                <p> Late: { this.state.late } </p>
 
             </div>
         );
--- src/components/University/SoloUniversity/SoloUniversity.js
+++ src/components/University/SoloUniversity/SoloUniversity.js
@@ -40,28 +40,6 @@ class SoloUniversity extends React.Component {
         }
     }
 
-    // componentDidMount() {
-    //     // debugger;
-    //     UniversityServices.getAllData()
-    //         .then(response => {
-    //             let university = response.data;
-    //             if (university) {
-    //                 this.setState({
-    //                     university
-    //                 });
-    //                 //console.log(university);
-    //             }
-    //             const foundUniversity = this.state.university.find(() => university.universityId === this.props.match.params.universityId);
-    //             //console.log(foundUniversity);
-    //             this.setState({
-    //                 foundUniversity
-    //             });
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    // }
-
     render() {
         const { name, info, image } = this.state.foundUniversity;
         let students = this.state.students.filter(student => student.universityId == this.props.match.params.universityId);
@@ -71,10 +49,10 @@ class SoloUniversity extends React.Component {
                     { this.state.update ? <div className="soloUniversity">
                         <h1>{ name }</h1>
                         <img src={ image }/>
-                        <p>Address: { info.address }</p>
-                        <p>Established: { info.established }</p>
-                        <p>President: { info.president }</p>
-                        <div><p>Students:</p> { students.map(student =>
+                        <p><strong>Address:</strong> { info.address }</p>
+                        <p><strong>Established:</strong> { info.established }</p>
+                        <p><strong>President:</strong> { info.president }</p>
+                        <div className='studentsAtUniversity'><h3>Students:</h3> { students.map(student =>
                             <p> { student.firstName } { student.lastName } </p>) } </div>
                     </div> : null }
 
